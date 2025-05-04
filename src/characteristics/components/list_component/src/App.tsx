0e182faf296907fa967827c85f41f1bb26dd2ebb
sarasds <alu0101484570@ull.edu.es>
2025-05-04 12:45:15 +0000

    components

---
+++ src/characteristics/components/list_component/src/App.tsx
@@ -0,0 +1,12 @@
+import ListComponent from './ListComponent'
+import './App.css'
+
+function App() {
+ return (<div>
+  <ListComponent/>
+  <ListComponent/>
+  <ListComponent/>
+  </div>);
+}
+
+export default App
---
+++ src/characteristics/components/list_component/src/ListComponent.tsx
@@ -0,0 +1,14 @@
+function ListComponent() {
+  return (
+    <div>
+      <h1>List Component</h1>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ListComponent;
